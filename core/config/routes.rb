05d01be7010fb735914db329cb8db4232466baf6
Jose Manuel Pérez González <josemanuel.perez@betybyte.com>
2012-09-21 09:40:45 +0200

    Issue #5866 Change /refinery by a configurable route, for the access to the admin.

--- core/config/routes.rb
+++ core/config/routes.rb
@@ -1,14 +1,14 @@
 Refinery::Core::Engine.routes.draw do
   filter(:refinery_locales) if defined?(RoutingFilter::RefineryLocales) # optionally use i18n.
   get 'wymiframe(/:id)', :to => 'fast#wymiframe', :as => :wymiframe
-  get 'refinery/message', :to => 'fast#message', :as => :message
+  get "#{Refinery::Core.config.admin_prefix_path}/message", :to => 'fast#message', :as => :message
 
-  namespace :admin, :path => 'refinery' do
+  namespace :admin, :path => Refinery::Core.config.admin_prefix_path do
     root :to => 'dashboard#index'
     resources :dialogs, :only => [:index, :show]
   end
 
-  match '/refinery/update_menu_positions', :to => 'admin/core#update_plugin_positions'
+  match "/#{Refinery::Core.config.admin_prefix_path}/update_menu_positions", :to => 'admin/core#update_plugin_positions'
 
   get '/sitemap.xml' => 'sitemap#index', :defaults => { :format => 'xml' }
 end
--- images/config/routes.rb
+++ images/config/routes.rb
@@ -1,7 +1,7 @@
 Refinery::Core::Engine.routes.prepend do
   match '/system/images/*dragonfly', :to => Dragonfly[:refinery_images]
 
-  namespace :admin, :path => 'refinery' do
+  namespace :admin, :path => Refinery::Core.config.admin_prefix_path do
     resources :images, :except => :show do
       get :insert, :on => :collection
     end
